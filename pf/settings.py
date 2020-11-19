185747cdd211dfc8a2411ab55cdf7732d1351942
saishashidhar <53369848+Saishashidhar66@users.noreply.github.com>
2020-11-19 13:17:51 +0530

    Update settings.py

--- pf/settings.py
+++ pf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -133,3 +134,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+django_heroku.settings(locals())
